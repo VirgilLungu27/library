72c44e8fbc1c88eb4041a615bd4dbe5274bd3635
Virgil <lunguvirgil3000@gmail.com>
2022-08-23 10:44:16 +0300

    Add loop through function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const table = document.querySelector('table')
+
+
 let myLibrary = [];
 
 const book = function Book(author, title, pages, read) {
@@ -12,7 +15,22 @@ function addBookToLibrary() {
 }
 
 function loopThrough {
-    for (book in myLibrary) {
-        
+    for (books in myLibrary) {
+       let newBook = document.createElement('div');
+       let text = `<div class="table">
+       <div class="author">
+             <h4>Author</h4>
+         </div>
+             <div class="book">
+             <h4>Book</h4>
+         </div>
+         <div class="pages">
+             <h4>Pages</h4>
+         </div>
+         <div class="read">
+             <h4>Read?</h4>
+         </div>
+     </div>`
+        newBook.appendChild(text);
     }
 }

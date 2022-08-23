781709d904e2d1fbdff5e25bee6d6b5fd260a245
Virgil <lunguvirgil3000@gmail.com>
2022-08-23 09:16:12 +0300

    create js file with book function

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read ? "true" : "false";
+}
+
+function addBookToLibrary() {
+
+}
